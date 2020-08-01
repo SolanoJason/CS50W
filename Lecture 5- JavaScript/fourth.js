07c3e708314e432d37dfe8f6ff53ca973a0128b9
SolanoJason <pajason2000@outlook.com>
2020-07-31 21:01:08 -0500

    Onclick.html/js

--- Lecture 5- JavaScript/fourth.js
+++ Lecture 5- JavaScript/fourth.js
@@ -1,2 +1,10 @@
-var s = "AnyString";
-console.log("oas");
+var a = 5;
+var b = 10;
+
+function foo(strings, ...values) {
+    let y = ("asd","asdasd");
+    return y;
+}
+
+const x = foo`Sum ${a + b} Product ${a * b} Division ${b / a}`;
+console.log(x);
